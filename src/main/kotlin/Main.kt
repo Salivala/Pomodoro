852a28c37608d746dfc7052f96ffd663e0395694
hsszyman <hsszyman@gmail.com>
2019-05-18 20:00:30 -0400

    Testing Concurrency

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
+import kotlinx.coroutines.*
 import java.time.Duration
 fun main() {
     println("testing")
-    var tray = TrayManager()
-    var pomodoro = PomodoroTimer(
-        secondAction = {timeLeft -> tray.text = timeLeft.toSeconds().toString()},
-        startBreak = {SoundPlayer().BeginBreak(); Duration.ofSeconds(5)},
-        startWork = {SoundPlayer().BeginWork(); Duration.ofSeconds(30)}
-    )
-    pomodoro.run()
+    val tray = TrayManager()
+    GlobalScope.launch {
+        val pomodoro = PomodoroTimer(
+            secondAction = { timeLeft -> tray.text = timeLeft.toSeconds().toString() },
+            startBreak = { SoundPlayer().BeginBreak(); Duration.ofSeconds(5) },
+            startWork = { SoundPlayer().BeginWork(); Duration.ofSeconds(30) }
+        )
+        pomodoro.run()
+    }
+    println("testing")
 }
